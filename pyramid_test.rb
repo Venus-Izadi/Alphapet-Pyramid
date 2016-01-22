0641432c9ef1a3c826de33d82f5de958dbf0c71a
Venus Izadi <venus.izadi@gmail.com>
2016-01-23 09:00:16 +1100

    First commit

---
+++ pyramid_test.rb
@@ -0,0 +1,23 @@
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'pyramid'
+
+class AlphabetPyramidTest < Minitest::Test
+  def does_not_get_numbers
+    #
+  end
+
+  def gets_letters_only
+  end
+
+  def output_is_uppercase
+  end
+
+  def does_not_get_invalid_input
+  #   return "INVALID INPUT"
+  end
+
+
+
+end
