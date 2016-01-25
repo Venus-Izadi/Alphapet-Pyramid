de1981bfb98242a03ba223ce00ccb4c528d5f574
Venus Izadi <venus.izadi@gmail.com>
2016-01-25 18:07:21 +1100

    drawing the whole diamond (broken) but quarters are fine

--- pyramid.rb
+++ pyramid.rb
@@ -1,18 +1,25 @@
 class Pyramid
 
-
-  def top_half_of_diamond(letter)
+  def draw_diamond(letter)
     letters = ('A'..letter).to_a
     spaces = (0..(letters.count - 1 )).to_a
 
     first_quarter = Hash[spaces.reverse.zip(letters)]
-    first_quarter.each {|key, value| puts ('_' * "#{key}".to_i) + "#{value}" }
+    draw(first_quarter)
 
     third_quarter = Hash[spaces.zip(letters.reverse)]
+    draw(third_quarter)
 
-    second_quarter = Hash[spaces.reverse.zip(letters)]
+    second_quarter = Hash[spaces.zip(letters)]
+    draw(second_quarter)
+
     fourth_quarter = Hash[spaces.reverse.zip(letters.reverse)]
+    draw(fourth_quarter)
   end
 
+  private
 
+  def draw(first_quarter)
+    first_quarter.each { |key, value| puts ('_' * "#{key}".to_i) + "#{value}" }
+  end
 end
