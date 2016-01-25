04bb309fbd8095c9c36fafebc271c67aab1d87a4
Venus Izadi <venus.izadi@gmail.com>
2016-01-25 14:31:43 +1100

    make left half of diamond

--- pyramid.rb
+++ pyramid.rb
@@ -1,22 +1,18 @@
 class Pyramid
 
-  def self.first_quarter_of_diamond(letter)
-    letters = ('A'..letter).to_a
-    number_of_rows = letters.count
-    1.upto(number_of_rows) do |i|
-      spaces = ' ' * (number_of_rows-i)
-      letter = letters[i-1]
-      puts spaces + letter + spaces
-    end
-  end
+  def self.left_half_of_diamond(letter)
+    @letters = ('A'..letter).to_a
+    @number_of_rows = @letters.count
 
-  def self.second_quarter_of_diamond(letter)
-    letters = ('A'..letter).to_a
-    number_of_rows = letters.count
-    number_of_rows.downto(1) do |i|
-      spaces = ' ' * (number_of_rows-i)
-      letter = letters[number_of_rows-i]
-      puts spaces + letter + spaces
+    1.upto(@number_of_rows) do |i|
+      spaces = ' ' * (@number_of_rows-i)
+      first_quarter_letter = @letters[i-1]
+      puts spaces + first_quarter_letter + spaces
+    end
+    (@number_of_rows-1).downto(1) do |i|
+      spaces = ' ' * (@number_of_rows-i)
+      third_quarter_letter = @letters[i-1]
+      puts spaces + third_quarter_letter + spaces
     end
   end
 end
