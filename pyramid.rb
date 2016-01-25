5319629e83bca3ecea465193862402880ece2ab1
Venus Izadi <venus.izadi@gmail.com>
2016-01-25 14:47:31 +1100

    make right half of the diamond

--- pyramid.rb
+++ pyramid.rb
@@ -15,4 +15,20 @@ class Pyramid
       puts spaces + third_quarter_letter + spaces
     end
   end
+
+  def self.right_half_of_diamond(letter)
+    @letters = ('A'..letter).to_a
+    @number_of_rows = @letters.count
+
+    (@number_of_rows-1).downto(2) do |i|
+      spaces = ' ' * (@number_of_rows-i)
+      first_quarter_letter = @letters[@number_of_rows-i]
+      puts spaces + first_quarter_letter + spaces
+    end
+    1.upto(@number_of_rows-1) do |i|
+      spaces = ' ' * (@number_of_rows-i)
+      third_quarter_letter = @letters[@number_of_rows-i]
+      puts spaces + third_quarter_letter + spaces
+    end
+  end
 end
