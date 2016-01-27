c864d10b979a8ee34b7a3eab370fa724b97dab7e
Venus Izadi <venus.izadi@gmail.com>
2016-01-27 16:04:11 +1100

    refactoring

--- pyramid.rb
+++ pyramid.rb
@@ -1,38 +1,33 @@
 class Pyramid
 
   def initialize(letter)
-   @letter = letter
+   @letters = ('A'..letter).to_a
+   @spaces = (0..(@letters.count - 1 )).to_a
   end
 
   def draw_diamond
-    @letters = ('A'..@letter).to_a
-    spaces = (0..(@letters.count - 1 )).to_a
+    first_quarter = Hash[@spaces.reverse.zip(@letters)]
+    second_quarter = Hash[@spaces.zip(@letters)]
+    draw(first_quarter, second_quarter)
 
-    first_quarter = Hash[spaces.reverse.zip(@letters)]
-    second_quarter = Hash[spaces.zip(@letters)]
-    make_line(first_quarter, second_quarter)
-
-    third_quarter = Hash[spaces.zip(@letters.reverse)]
-    fourth_quarter = Hash[spaces.reverse.zip(@letters.reverse)]
-    make_line(third_quarter, fourth_quarter)
+    third_quarter = Hash[@spaces.zip(@letters.reverse)]
+    fourth_quarter = Hash[@spaces.reverse.zip(@letters.reverse)]
+    draw(third_quarter, fourth_quarter)
   end
 
   private
 
-  def draw(space, letter)
-     print ' ' * space + letter + ' ' * (@letters.count - space -1)
-  end
-
-  def make_line(hash1, hash2)
+  def draw(quarter_one, quarter_two)
     0.upto(@letters.count - 1) do |i|
-      spaces = hash1.keys[i]
-      letter = hash1.values[i]
-      draw(spaces,letter)
-      current_spaces = hash2.keys[i]
-      current_letter = hash2.values[i]
-      draw(current_spaces,current_letter)
+      make_line(i, quarter_one)
+      make_line(i, quarter_two)
       puts
     end
   end
 
+  def make_line(i, quarter_one)
+    spaces = quarter_one.keys[i]
+    letter = quarter_one.values[i]
+    print (' ' * spaces) + letter + ' ' * (@letters.count - spaces -1)
+  end
 end
