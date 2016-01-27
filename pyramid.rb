75e7ec2894ec7e1ef368c31040e80c63d1f25115
Venus Izadi <venus.izadi@gmail.com>
2016-01-27 15:54:01 +1100

    diamond is right - 4 quarters are glued to eachother

--- pyramid.rb
+++ pyramid.rb
@@ -1,25 +1,38 @@
 class Pyramid
 
-  def draw_diamond(letter)
-    letters = ('A'..letter).to_a
-    spaces = (0..(letters.count - 1 )).to_a
+  def initialize(letter)
+   @letter = letter
+  end
 
-    first_quarter = Hash[spaces.reverse.zip(letters)]
-    draw(first_quarter)
+  def draw_diamond
+    @letters = ('A'..@letter).to_a
+    spaces = (0..(@letters.count - 1 )).to_a
 
-    third_quarter = Hash[spaces.zip(letters.reverse)]
-    draw(third_quarter)
+    first_quarter = Hash[spaces.reverse.zip(@letters)]
+    second_quarter = Hash[spaces.zip(@letters)]
+    make_line(first_quarter, second_quarter)
 
-    second_quarter = Hash[spaces.zip(letters)]
-    draw(second_quarter)
-
-    fourth_quarter = Hash[spaces.reverse.zip(letters.reverse)]
-    draw(fourth_quarter)
+    third_quarter = Hash[spaces.zip(@letters.reverse)]
+    fourth_quarter = Hash[spaces.reverse.zip(@letters.reverse)]
+    make_line(third_quarter, fourth_quarter)
   end
 
   private
 
-  def draw(first_quarter)
-    first_quarter.each { |key, value| puts ('_' * "#{key}".to_i) + "#{value}" }
+  def draw(space, letter)
+     print ' ' * space + letter + ' ' * (@letters.count - space -1)
   end
+
+  def make_line(hash1, hash2)
+    0.upto(@letters.count - 1) do |i|
+      spaces = hash1.keys[i]
+      letter = hash1.values[i]
+      draw(spaces,letter)
+      current_spaces = hash2.keys[i]
+      current_letter = hash2.values[i]
+      draw(current_spaces,current_letter)
+      puts
+    end
+  end
+
 end
